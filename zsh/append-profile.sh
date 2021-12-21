473a9dff60d9d38e00b9590b2fe10f132f998bab
hammerfunctor <topo20@protonmail.com>
2021-12-22 01:10:28 +0800

    refactor zsh configs, add alacritty

---
+++ zsh/append-profile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+echo "export LC_CTYPE=en_US.UTF-8" >> $HOME/.zprofile
+echo "export LC_ALL=en_US.UTF-8" >> $HOME/.zprofile
+echo "export LANG=en_US.UTF-8" >> $HOME/.zprofile
+echo "export EDITOR=/usr/bin/vim" >> $HOME/.zprofile
