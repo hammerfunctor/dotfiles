8de5b761a1cb613fcc6a41c6f413b7a78ad70c78
hammerfunctor <topo20@protonmail.com>
2021-12-25 00:51:22 +0800

    fix a bug

--- setups/homebrew.sh
+++ setups/homebrew.sh
@@ -2,5 +2,4 @@
 
 [ "$(uname)" =~ "Darwin" ] && echo "Not a Mac, not to install homebrew" && exit 0
 [ ! -z $(command -v brew) ] && echo "homebrew already installed." && echo 0
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
