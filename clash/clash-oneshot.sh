55d98cc2c80b9d771fcd878feca188cf27d04744
hammerfunctor <topo20@protonmail.com>
2021-12-25 00:25:28 +0800

    make a dir if there is not one

--- clash/clash-oneshot.sh
+++ clash/clash-oneshot.sh
@@ -3,14 +3,13 @@
 cfgfname="$HOME/.config/clash/config.yaml"
 srcfname="$(dirname $0)/ghelper-clash.txt"
 
-if ! [ -f $srcfname ]; then
-    echo "$srcfname not fount"
-    exit 1
-fi
+[ ! -d $(dirname $cfgfname) ] && mkdir -p $(dirname $cfgfname)
+[ ! -f $srcfname ] && echo "$srcfname not found." && exit 1
+
 curl $(cat $srcfname) -o $cfgfname
 
 sed -i 's/^external-contr.*$/external-controller: 127\.0\.0\.1:9090/' $cfgfname
 sed -i 's/^port: .*$/port: 7891/' $cfgfname
-sed -i 's/^socks-port: .*$/socks-port: 7890/' $cfgfname
+sed -i 's/^socks-port: .*$/socks-port: 7891/' $cfgfname
 sed -i 's/^mixed-port: .*$/mixed-port: 7891/' $cfgfname
 sed -i 's/^log-level: .*$/log-level: silent/' $cfgfname
