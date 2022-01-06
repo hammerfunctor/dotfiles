f1c90cb9ac925bf9c38adaf16f0821029f0d0137
hammerfunctor <topo20@protonmail.com>
2022-01-06 21:32:18 +0800

    clash: ip for new config 127.0.0.1 => 0.0.0.0

--- clash/clash-oneshot.sh
+++ clash/clash-oneshot.sh
@@ -8,8 +8,23 @@ srcfname="$(dirname $0)/ghelper-clash.txt"
 
 curl $(cat $srcfname) -o $cfgfname
 
-sed -i 's/^external-contr.*$/external-controller: 127\.0\.0\.1:9090/' $cfgfname
-sed -i 's/^port: .*$/port: 7891/' $cfgfname
-sed -i 's/^socks-port: .*$/socks-port: 7891/' $cfgfname
-sed -i 's/^mixed-port: .*$/mixed-port: 7891/' $cfgfname
-sed -i 's/^log-level: .*$/log-level: silent/' $cfgfname
+sed -i \
+  -e 's/^external-contr.*$/external-controller: 127\.0\.0\.1:9090/' \
+  -e 's/^port: .*$/port: 7891/' \
+  -e 's/^socks-port: .*$/socks-port: 7891/' \
+  -e 's/^mixed-port: .*$/mixed-port: 7891/' \
+  -e 's/^log-level: .*$/log-level: silent/' \
+  -e 's/^bind-address: .*$/bind-address: 0.0.0.0/' \
+  $cfgfname
+
+
+# sometimes we want to proxy all connections, especially for steam community
+# so we create an extra config file to match all url with a first 'MATCH'
+globalname=$(dirname $cfgfname)/config.global.yaml
+sed \
+  -e 's/^port: .*$/port: 7892/' \
+  -e 's/^socks-port: .*$/socks-port: 7892/' \
+  -e 's/^mixed-port: .*$/mixed-port: 7892/' \
+  -e 's/type: select/type: url-test\n  url: http:\/\/www\.gstatic\.com\/generate_204\n  interval: 600/' \
+  -e 's/rules:.*/rules:\n- MATCH,Ghelper/' \
+  $cfgfname > $globalname
