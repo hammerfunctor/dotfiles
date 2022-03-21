e1c65c891110c1c6ed06026b39d0d05dffee2e89
hammerfunctor <topo20@protonmail.com>
2022-03-21 14:08:26 +0800

    rename config to config.lua

--- config.lua
+++ config.lua
@@ -5,10 +5,12 @@ links = {
   {src='nvim', dst='~/.config/nvim', mode='sub'},
   {src='texmacs', dst='~/.TeXmacs', mode='children'},
   {src='zsh/zshrc', dst='~/.zshrc'},
-  {src='gpg-agent.conf', dst='~/.gnupg/gpg-agent.conf'}
+  {src='gpg-agent.conf', dst='~/.gnupg/gpg-agent.conf'},
+  {src='kde/user-dirs.dirs', dst='~/.config/user-dirs.dirs'},
+  {src='kde/user-dirs.locale', dst='~/.config/user-dirs.locale'}
 }
 
 scripts = {
-  'clash/clash-oneshot.sh',
+--  'clash/clash-oneshot.sh',
 }
 
