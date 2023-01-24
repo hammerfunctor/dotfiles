39acaa310174bf16b088bbc2522eb7312085884b
hammerfunctor <topo20@protonmail.com>
2023-01-24 00:12:42 -0500

    factor out kde configs

--- kde/onbattery.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# disable file indexer
-balooctl disable
--- kde/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-dir="$(dirname $0)"
-
-balooctl disable
-
-cp "$dir/user-dirs.dirs" "~/.config/user-dirs.dirs"
-cp "$dir/user-dirs.locale" "~/.config/user-dirs.locale"
