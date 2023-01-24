502dca60de4b232cf70d44f9026a7f3c4d473f90
hammerfunctor <topo20@protonmail.com>
2023-01-24 00:50:14 -0500

    texmacs: remove individual scm scripts

--- texmacs/progs/my-init-buffer.scm
+++
@@ -1,9 +0,0 @@
-(if (not (buffer-has-name? (current-buffer)))
-    (begin
-      ;; style and packages
-      (init-style "mynotes")
-      (buffer-pretend-saved (current-buffer))
-
-      ))
-
-;;(init-page-rendering "papyrus")
