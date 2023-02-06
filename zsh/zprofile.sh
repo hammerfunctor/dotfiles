0eeca48f5845854840f12a2554ca03937e6356b3
hammerfunctor <topo20@protonmail.com>
2023-02-06 16:45:23 -0500

    routine update

--- zsh/zprofile.sh
+++ zsh/zprofile.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/sh
 
 echo "
-P_IP='127.0.0.1'
-P_PORT='7891'
-SK_PORT='7891'
-#export HTTP_PROXY=socks5://\$P_IP:\$P_PORT/ HTTPS_PROXY=socks5://\$P_IP:\$P_PORT/
-
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
